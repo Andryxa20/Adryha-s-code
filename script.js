f8d0ae3c6928f3776fc8696a141158c658296375
Andryxa20 <andreeeeyyytss@gmail.com>
2025-07-17 12:55:53 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() { // Коли сторінка повністю завантажилась
-  const btn = document.getElementById('sayHiBtn'); // Знаходимо кнопку за id
-  if (btn) { // Якщо кнопка існує
-    btn.addEventListener('click', function() { // Додаємо обробник події "клік"
-      alert("Привіт! Я Андрій, і я тільки починаю — але у мене все вийде!"); // Показати повідомлення
-    });
-  }
-});
